4b1dcee1166283a82522cc5515d8d2b294045c48
Martin <25070051+anquetil@users.noreply.github.com>
2023-12-19 12:04:14 +0100

    new addresses

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -3,18 +3,17 @@ import { Address } from 'viem'
 export const testSendETHAddressGoerli =
    '0x6ab075abfA7cdD7B19FA83663b1f2a83e4A957e3' as Address
 
-export const deploys =
-   // dec7th
-   {
-      registryExtension: '0x26431F48C445A04bb526FE403247DEafb0006206',
-      equippableExtension: '0x46AE0962537593Ab92f5ec98D0D99bCE8f137de2',
-      easel: '0xB8e799e21BF8517e9FFE62370f3B1B102A507A7f',
-      tokenMetadataExtension: '0x3613eCBdDf54710de21df9fD684B21f349E6AdC9',
-      metadataExtension: '0xd3A8BB5A181993E869aC89bf6E66F68aEABDB7FF',
-      'NPC(721)': '0x4dD30A31962431da2e7359de2527eeD09902B65F',
-      'Trait(1155)': '0x34914c7c6Bd75c1C69e14F50CE5285c2F5335e5c',
-      railsTokenFactory: '0x2C333bd1316cE1aF9EBF017a595D6f8AB5f6BD1A',
-      erc6551Registry: '0x000000006551c19487814612e58FE06813775758',
-      erc6551AccountProxy: '0x55266d75D1a14E4572138116aF39863Ed6596E7F',
-      erc6551AccountImpl: '0x41C8f39463A868d3A88af00cd0fe7102F30E44eC',
-   }
+export const deploys = // dec19th
+{
+registryExtension: "0x8c3Ef0d04395861c31C2Fc2EE4c7F9bc9BE319B4" as Address,
+equippableExtension: "0x5924b8fC00b388BC1D6B4a4091253ec517e783b3" as Address,
+easel: "0xA074f5520B8A40A85c49f9EAA5B66915F89892db" as Address,
+tokenMetadataExtension: "0xE4AbEdA33F1B040AAd17Babd8dC9Ab6eB686AD58" as Address,
+metadataExtension: "0x3cA34E441F12c914f7B29A3F60604DBE410EC58f" as Address,
+"NPC(721)": "0xC2c16A16Bcb774663a84C44a960693E73F273617" as Address,
+"Trait(1155)": "0x810cdD881Db44eE29747CB44516fD69185e02b2F" as Address,
+railsTokenFactory: "0x2C333bd1316cE1aF9EBF017a595D6f8AB5f6BD1A" as Address,
+erc6551Registry: "0x000000006551c19487814612e58FE06813775758" as Address,
+erc6551AccountProxy: "0x55266d75D1a14E4572138116aF39863Ed6596E7F" as Address,
+erc6551AccountImpl: "0x41C8f39463A868d3A88af00cd0fe7102F30E44eC" as Address
+}
